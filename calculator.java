f1481c37e5d136206ce5a4614a125f34adf08810
Steven0f <42715105+Steven0f@users.noreply.github.com>
2018-10-13 10:49:44 -0400

    Add files via upload
    Program from CodeCademy to implement simple calculator.

---
+++ calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+  public static void Calculator(String[] args) {
+  }
+}
+
+int public add(int a b) {
+  return(a + b);
+}
+         
+int public subtract(int a, int b) {
+  return(a - b);
+}
+
+int public multiply(int a, int b) {
+  return(a * b);
+}
+int public add(int a, int b) {
+  if (b == 0)
+    System.out.println("Error: divide by zero.");
+    return(0);
+  else
+		return(a / b);
+}
+
+int public modulo(int a, int b)
